e5a03a2a64b0dbe0c14c47d2971cb7c378a41fc1
Ammar Ahmed <ammarahmed1263@gmail.com>
2025-11-11 21:32:24 +0200

    feat(swap_by_address): swap two ints using pointers

---
+++ OOP-C++/pointers_and_dynamic_memory/swap_by_address.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int *, int *);
+int main()
+{
+    int x, y;
+    cout << "Enter two integers: ";
+    cin >> x >> y;
+    swap(&x, &y);
+    cout << "x: " << x << ", y: " << y << endl;
+    return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
