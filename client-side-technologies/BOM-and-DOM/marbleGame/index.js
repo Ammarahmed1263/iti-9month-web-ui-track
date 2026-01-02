66b1b5bb8016dde30b07f1ca4c4ae7f15b71f5e8
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-01-02 02:14:35 +0200

    fix(marbleGame): correct event listener assignment and improve timer management

--- client-side-technologies/BOM-and-DOM/marbleGame/index.js
+++ client-side-technologies/BOM-and-DOM/marbleGame/index.js
@@ -6,18 +6,20 @@ let speed = 400;
 let step = -50;
 
 for (let i = 0; i < count; i++) {
-  document.images[i].mouseover = stop;
-  document.images[i].mouseout = start;
+  document.images[i].onmouseover = stop;
+  document.images[i].onmouseout = start;
 }
 
 function start() {
+  if (timerID) return;
+
   function callback() {
     document.images[index].src = "images/marble1.jpg";
     index = (index + 1) % count;
     document.images[index].src = "images/marble3.jpg";
 
     if (index === count - 1) {
-      clearInterval(timerID);
+      stop();
 
       if (speed <= 50) {
         step = 50;
@@ -29,15 +31,16 @@ function start() {
 
       timerID = setInterval(callback, speed);
     }
-
-    console.log(speed);
   }
 
   timerID = setInterval(callback, speed);
 }
 
 function stop() {
-  clearInterval(timerID);
+  if (timerID) {
+    clearInterval(timerID);
+    timerID = null;
+  }
 }
 
 start();
