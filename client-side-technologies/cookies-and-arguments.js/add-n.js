ca2c823c5ccc545557078392b23bdd784e88d708
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-01-06 23:46:35 +0200

    feat(add-params): implement function to sum parameters with validation

---
+++ client-side-technologies/cookies-and-arguments.js/add-n.js
@@ -0,0 +1,23 @@
+function addParams() {
+  if (arguments.length == 0) {
+    throw new Error("Expected at least 1 argument, but got 0");
+  }
+
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] !== "number") {
+      throw new Error(
+        "in argument " +
+          arguments[i] +
+          " expected a number but got " +
+          typeof arguments[i]
+      );
+    }
+
+    sum += arguments[i];
+  }
+
+  return sum;
+}
+
+console.log(addParams(1, -4, 5));
