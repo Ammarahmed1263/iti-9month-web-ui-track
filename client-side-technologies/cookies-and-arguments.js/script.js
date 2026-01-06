709de71ffa0a56dee51e340a5bfdbc8a6253fc8c
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-01-06 23:45:02 +0200

    feat(cookies): add HTML and JavaScript for cookie management functionality

---
+++ client-side-technologies/cookies-and-arguments.js/script.js
@@ -0,0 +1,32 @@
+// setCookie();
+// setCookie("userName", "profile", "1/11/2026", "extra");
+// setCookie(123, "val");
+// setCookie("", "val");
+// setCookie("userName;", "Ammar");
+// setCookie("user", "val", "invalid-date");
+// setCookie("complex Data", "test@email.com");
+
+setCookie("userName", "Ammar", "1/11/2026");
+setCookie("sessionUser", "SessionValue");
+
+// getCookie();
+// getCookie(1);
+// getCookie("nonExistent");
+
+console.log("Retrieved:", getCookie("userName"));
+// console.log("Retrieved:", getCookie("complex Data"));
+
+// deleteCookie();
+// deleteCookie(123);
+// deleteCookie("nonExistent");
+
+deleteCookie("userName");
+try {
+  getCookie("userName");
+} catch (e) {
+  console.log("Verification: Cookie deleted successfully");
+}
+
+showCookieList();
+console.log(hasCookie("sessionUser"));
+console.log(hasCookie("userName"));
