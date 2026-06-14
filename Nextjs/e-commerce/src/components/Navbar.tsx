0841e2a240d1909a025beb3ee91488cac47a8b36
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-06-14 20:45:18 +0300

    feat: implement navigation bar component

---
+++ Nextjs/e-commerce/src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Navbar = () => {
+  const router = useRouter();
+
+  return (
+    <header className='flex items-center justify-between gap-4 bg-card p-4 px-12 shadow-sm'>
+      <p className='text-xl font-bold text-primary tracking-tight'>
+        Essentials<span className='text-foreground'>.</span>
+      </p>
+      <ul className='flex space-x-6 text-foreground font-medium'>
+        <li>
+          <Link
+            href='/'
+            className={`transition-colors ${router.pathname === "/" ? "text-primary font-bold" : "text-muted-foreground hover:text-primary"}`}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href='/products'
+            className={`transition-colors ${router.pathname.startsWith("/products") ? "text-primary font-bold" : "text-muted-foreground hover:text-primary"}`}
+          >
+            Products
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Navbar;
